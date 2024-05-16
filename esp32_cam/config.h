ef3453dcbee73eb5d0ed8afe691d9f2d3ffbf55d
Ángel Garcia Beltrán <angelurano00@gmail.com>
2024-05-16 20:59:14 +0200

    add esp32cam firmware

---
+++ esp32_cam/config.h
@@ -0,0 +1,58 @@
+#include "esp_camera.h"
+
+#define CAMERA_MODEL_AI_THINKER
+#include "camera_pins.h"
+#define PIN_LED 2
+
+// Uncomment and add values to set default WiFi values
+// #define DEFAULT_SSID ""
+// #define DEFAULT_PASSWORD ""
+
+#define SOCKET_PORT 8000 // Port from the server
+
+// Uncomment and add value to set default host IP value
+// #define DEFAULT_SOCKET_HOST ""
+
+esp_err_t initCamera() { 
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_DRAM;
+
+  // Parameters for image quality and size
+  config.frame_size = FRAMESIZE_VGA; // FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
+  config.jpeg_quality = 15; //10-63 lower number means higher quality
+  config.fb_count = 2;
+  
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Inicio de Camara Fallado: 0x%x", err);
+    return err;
+  }
+
+  sensor_t * s = esp_camera_sensor_get();
+  int res = s->set_framesize(s, config.frame_size);
+
+  Serial.println("Inicio de Camara Correcto OK");
+  return ESP_OK;
+}
